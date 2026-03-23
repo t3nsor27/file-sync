896f7e6fbfffe273f802453d545f12d8a139416a
t3nsor27 <srikant.sahoo.144@gmail.com>
2026-03-23 15:19:49 +0530

    add resolve and connect in ui

--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
+#include <memory>
+#include <mutex>
 #include <string>
+#include <vector>
+#include "./include/net/peer.hpp"
 
-std::pair<std::string, std::string> GetHostInfo() {
+namespace asio = boost::asio;
+std::pair<std::string, asio::ip::address> GetHostInfo() {
   boost::asio::io_context io;
 
   std::string hostname = boost::asio::ip::host_name();
-  std::string ip = "unknown";
+  asio::ip::address ip;
 
   try {
     boost::asio::ip::udp::socket socket(io);
@@ -18,21 +23,83 @@ std::pair<std::string, std::string> GetHostInfo() {
     socket.connect(boost::asio::ip::udp::endpoint(
         boost::asio::ip::make_address("8.8.8.8"), 80));
 
-    ip = socket.local_endpoint().address().to_string();
+    ip = socket.local_endpoint().address();
   } catch (...) {
   }
 
   return {hostname, ip};
 }
 
-int main() {
+struct PeerInfo {
+  std::string name;
+  uint64_t peer_id;
+  asio::ip::address address;
+  uint16_t port;
+  std::weak_ptr<net::Session> session;
+};
+
+PeerInfo ExtractPeerInfo(std::shared_ptr<net::Session> session,
+                         const net::Session::HelloPacket& hello) {
+  auto& socket = session->socket();
+  auto endpoint = socket.remote_endpoint();
+
+  PeerInfo info;
+  info.address = endpoint.address();
+  info.port = endpoint.port();
+  info.session = session;
+
+  info.peer_id = hello.peer_id;
+  info.name = hello.hostname;
+
+  return info;
+}
+
+int main(int argc, char* argv[]) {
   using namespace ftxui;
-  namespace asio = boost::asio;
+
+  if (argc != 2) {
+    return 0;
+  }
 
   auto screen = ScreenInteractive::Fullscreen();
   auto bg_color = Color::Palette256(16);
 
-  auto [hostname, ip] = GetHostInfo();
+  auto [hostname, ip_addr] = GetHostInfo();
+
+  std::string ip = ip_addr.to_string();
+  std::string peer_ip, peer_port, peer_timeout;
+  std::mutex peer_mutex;
+  std::vector<PeerInfo> peer_list;
+  uint16_t port = std::stoi(argv[1]);
+  auto peer = std::make_shared<net::Peer>(port);
+  PeerInfo local_peer{hostname, peer->id(), ip_addr, port};
+
+  // -------------------------------------------------------------------------------------------------
+  // ACCEPT LOOP
+  // -------------------------------------------------------------------------------------------------
+
+  peer->doAccept([&](std::weak_ptr<net::Session> weak_session) {
+    if (auto session = weak_session.lock()) {
+      asio::co_spawn(
+          peer->getExecutor(),
+          [&, session]() -> asio::awaitable<void> {
+            co_await session->sendHello({peer->id(), hostname});
+            auto hello = co_await session->receiveHello();
+            auto info = ExtractPeerInfo(session, hello);
+            {
+              std::lock_guard<std::mutex> lock(peer_mutex);
+              peer_list.push_back(std::move(info));
+            }
+
+            screen.PostEvent(Event::Custom);
+          },
+          asio::detached);
+    }
+  });
+
+  std::thread io_thread([peer]() {
+    peer->run();
+  });
 
   // -------------------------------------------------------------------------------------------------
   // STATUS BAR
@@ -43,16 +110,18 @@ int main() {
     };
 
     std::string separator = "  ";
+    std::string peer_no =
+        peer_list.size() > 0 ? std::to_string(peer_list.size()) : "no";
 
-    return hbox({text("file-sync") | color(Color::White) | bold,
-                 filler(),
-                 dimText("no peer connected"),
-                 dimText(separator),
-                 dimText(hostname),
-                 dimText(separator),
-                 dimText(ip),
-                 dimText(separator),
-                 dimText("awaiting connection")}) |
+    return hbox({
+               text("file-sync") | color(Color::White) | bold,
+               filler(),
+               dimText(peer_no + " peer connected"),
+               dimText(separator),
+               dimText(hostname),
+               dimText(separator),
+               dimText(ip + ':' + std::to_string(port)),
+           }) |
            xflex | bgcolor(Color::Black) | borderHeavy;
   });
 
@@ -83,17 +152,41 @@ int main() {
 
   auto peer_connect_button = Button({
       .label = "CONNECT",
-      .on_click = []() {},
+      .on_click =
+          [&, peer]() {
+            if (peer_ip.empty() || peer_port.empty())
+              return;
+            uint16_t port = std::stoi(peer_port);
+            peer->doResolveAndConnect(
+                peer_ip, port, [&](std::weak_ptr<net::Session> ws) {
+                  if (auto session = ws.lock()) {
+                    asio::co_spawn(
+                        peer->getExecutor(),
+                        [&, session]() -> asio::awaitable<void> {
+                          auto hello = co_await session->receiveHello();
+                          co_await session->sendHello({peer->id(), hostname});
+                          auto info = ExtractPeerInfo(session, hello);
+                          {
+                            std::lock_guard<std::mutex> lock(peer_mutex);
+                            peer_list.push_back(std::move(info));
+                          }
+
+                          screen.PostEvent(Event::Custom);
+                        },
+                        asio::detached);
+                  }
+                });
+          },
       .transform =
           [&](EntryState state) {
+            auto button = text("CONNECT") | center;
             if (state.focused) {
-              return text("CONNECT") | center | borderDouble;
+              return button | borderDouble;
             }
-            return text("CONNECT") | center | borderHeavy;
+            return button | borderHeavy;
           },
   });
 
-  std::string peer_ip, peer_port, peer_timeout;
   auto peer_ip_input = peer_input(&peer_ip, "IP Address");
   auto peer_port_input = peer_input(&peer_port, "Port");
   auto peer_timeout_input = peer_input(&peer_timeout, "Timeout");
@@ -101,8 +194,8 @@ int main() {
   auto peer_container = Container::Vertical({
       peer_ip_input,
       Container::Horizontal({
-          peer_port_input,
-          peer_timeout_input,
+          peer_port_input | flex,
+          peer_timeout_input | flex,
       }),
       peer_connect_button,
   });
@@ -120,15 +213,14 @@ int main() {
 
   // NOTE: Try to implement a Vertical underline menu
   int selected_peer = 0;
-  auto connected_peer_menu_entry = [&selected_peer, bg_color](
-                                       std::string peer_name,
-                                       asio::ip::address peer_address,
-                                       uint16_t peer_port) {
+  auto connected_peer_menu_entry = [&selected_peer,
+                                    bg_color](const PeerInfo& info) {
     MenuEntryOption entry_option;
+    std::string peer_name = info.name;
     entry_option.label = peer_name;
     std::string address =
-        peer_address.to_string() + ":" + std::to_string(peer_port);
-    entry_option.transform = [&selected_peer, peer_name, address, bg_color](
+        info.address.to_v4().to_string() + ":" + std::to_string(info.port);
+    entry_option.transform = [&selected_peer, address, peer_name, &bg_color](
                                  EntryState state) {
       // clang-format off
       auto peer_info = vbox({
@@ -161,21 +253,24 @@ int main() {
     return entry;
   };
 
-  auto connected_peer_container = Container::Vertical(
-      {
-          connected_peer_menu_entry(
-              "Peer1", asio::ip::make_address("192.160.122.1"), 9000),
-          connected_peer_menu_entry(
-              "Peer2", asio::ip::make_address("192.160.122.2"), 9000),
-          connected_peer_menu_entry(
-              "Peer3", asio::ip::make_address("192.160.122.3"), 9000),
-      },
-      &selected_peer);
+  auto connected_peer_container = Container::Vertical({}, &selected_peer);
+
+  auto rebuild_peer_list = [&]() {
+    connected_peer_container->DetachAllChildren();
+
+    std::lock_guard<std::mutex> lock(peer_mutex);
+    for (auto& info : peer_list) {
+      connected_peer_container->Add(connected_peer_menu_entry(info));
+    }
+  };
 
   auto connected_peer_renderer = Renderer(connected_peer_container, [&]() {
+    rebuild_peer_list();
     return vbox({text("CONNECTED PEERS") | bold | dim | center,
                  separatorLight(),
-                 connected_peer_container->Render() | frame}) |
+                 connected_peer_container->ChildCount() != 0
+                     ? connected_peer_container->Render() | frame
+                     : text("NO PEER CONNECTED")}) |
            borderLight;
   });
 
@@ -183,21 +278,27 @@ int main() {
   // MAIN UI
   // -------------------------------------------------------------------------------------------------
 
-  auto ui = Container::Vertical(
-                {status_bar_renderer,
-                 Container::Horizontal(
-                     {Container::Vertical(
-                          {peer_renderer, connected_peer_renderer | flex}),
-                      Renderer([&]() {
-                        return vbox({text("IP is: " + peer_ip),
-                                     text("Port is: " + peer_port),
-                                     text("Timeout is: " + peer_timeout),
-                                     text("Selected_peer: " +
-                                          std::to_string(selected_peer))}) |
-                               flex | border;
-                      })}) |
-                     flex}) |
-            bgcolor(bg_color);
+  // clang-format off
+  auto ui = Container::Vertical({
+    status_bar_renderer,
+    Container::Horizontal({
+      Container::Vertical({
+        peer_renderer, 
+        connected_peer_renderer | flex
+      }) | size(WIDTH, GREATER_THAN, 30),
+        Renderer([&]() {
+          return vbox({
+          text("IP is: " + peer_ip),
+          text("Port is: " + peer_port),
+          text("Timeout is: " + peer_timeout),
+          text("Selected_peer: " +
+              std::to_string(selected_peer))}) | flex | border;
+        })
+    }) | flex
+  }) | bgcolor(bg_color);
+  // clang-format on
 
   screen.Loop(ui);
+  peer->stop();
+  io_thread.join();
 }
